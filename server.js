6e6e938942f4c0dd7a3b01bd5c65710c9ffb20b7
elit-altum <manan.sharma311@gmail.com>
2020-06-13 13:09:06 +0530

    refactor: changed API server to 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Use this file only for the server
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
 	console.log("Server is running on port:", port);
