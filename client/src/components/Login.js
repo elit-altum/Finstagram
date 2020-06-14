e40790a5111fb61490055a7191ae8ab394722aba
elit-altum <manan.sharma311@gmail.com>
2020-06-14 18:13:58 +0530

    add: react-toastify and login form styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 
+import { toast } from "react-toastify";
+
 const LoginForm = () => {
 	const sendFormData = async (e) => {
 		e.preventDefault();
@@ -16,29 +18,41 @@ const LoginForm = () => {
 					password,
 				},
 			});
-		} catch (err) {}
+			toast.success("Logged in successfully!", {
+				autoClose: 2000,
+			});
+			setTimeout(() => {
+				window.location.reload(true);
+			}, 2000);
+		} catch (err) {
+			console.log(err.response.data.data.error.message);
+			toast.error(`Error: ${err.response.data.data.error.message}`);
+		}
 	};
 	return (
-		<div className="login-form">
-			<form onSubmit={sendFormData}>
-				<label>Username</label>
-				<input
-					type="text"
-					name="username"
-					required={true}
-					placeholder="username"
-					id="username_field"
-				></input>
-				<label>Password</label>
-				<input
-					type="password"
-					name="password"
-					placeholder="********"
-					id="password_field"
-					required={true}
-				></input>
-				<button type="submit">Submit</button>
-			</form>
+		<div className="login-page">
+			<div className="login-form-container">
+				<h2>Finstagram</h2>
+				<form onSubmit={sendFormData} class="login-form">
+					<input
+						type="text"
+						name="username"
+						required={true}
+						placeholder="username"
+						id="username_field"
+					></input>
+					<input
+						type="password"
+						name="password"
+						placeholder="********"
+						id="password_field"
+						required={true}
+					></input>
+					<button type="submit" class="login-form__submit">
+						Submit
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import AppRouter from "./router/router";
 
 import "./styles/index.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+const jsx = (
+	<div>
+		<ToastContainer
+			position="top-right"
+			autoClose={5000}
+			hideProgressBar={false}
+			newestOnTop={true}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover
+		/>
+		<AppRouter />
+	</div>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
